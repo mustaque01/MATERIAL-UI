8fe4521b37d7d9ac6dc02c2e889b804fd1cb4072
Md Mustak <mustakarman560@gmail.com>
2025-06-28 14:20:44 +0530

    api calling

--- mini-project-react/src/searchBox.jsx
+++ mini-project-react/src/searchBox.jsx
@@ -1,13 +1,31 @@
 import TextField from '@mui/material/TextField';
 
+import Button from '@mui/material/Button';
+import "./SearchBox.css";
+
 
 
 export default function SearchBox({ searchTerm, setSearchTerm }) {
+    let [city, setCity] = useState("");
+
+    let handleSubmit = (e) => {
+        e.preventDefault();
+        setSearchTerm(city);
+        setCity("");
+    };
+
+
+
+
   return (
     <div className="search-box">
       <h3>Search for the weather</h3>
-      <form>
-        <TextField id="outlined-basic" label="search" variant="outlined" />
+      <form onSubmit={handleSubmit}>
+        <TextField id="City" label="City Name" variant="outlined" required value={city} onChange={(e) => setCity(e.target.value)} />
+        <br></br>
+      <Button variant="contained" type="submit">
+        Search
+      </Button>
       </form>
     </div>
   );
